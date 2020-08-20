11172088d0170aa95315276dbb8ba6ef839dd7d0
oktroco <andyoh12@naver.com>
2020-08-20 20:25:46 +0900

    MainScreen과 폴더구조 작성

---
+++ src/screens/MainScreen.tsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+class MainScreen extends Component<{}, {}> {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text style={{color: 'black'}}>Hello</Text>
+      </View>
+    );
+  }
+}
+
+export default MainScreen;
